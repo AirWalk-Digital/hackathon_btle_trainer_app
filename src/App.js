70281c8e70d4f1a2b9a3c20c9a3a68015eaaca70
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2021-12-20 12:52:31 +0000

    Set via variable

--- src/App.js
+++ src/App.js
@@ -16,6 +16,30 @@ import BluetoothDisabledRoundedIcon from "@mui/icons-material/BluetoothDisabled"
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import Stack from "@mui/material/Stack";
 
+let targetPowerLevel = 0;
+
+function requestControl() {
+  const OpCode = 0x00;
+  let buffer = new ArrayBuffer(1);
+  let view = new DataView(buffer);
+  view.setUint8(0, OpCode, true);
+
+  return view;
+}
+
+function powerTarget(args) {
+  console.log(args);
+  const OpCode = 0x05;
+  const power = args.power;
+
+  const buffer = new ArrayBuffer(3);
+  const view = new DataView(buffer);
+  view.setUint8(0, OpCode, true);
+  view.setUint16(1, power, true);
+
+  return view;
+}
+
 const MyChart = ({ data }) => {
   return (
     <ResponsiveContainer height={"100%"} width={"100%"} aspect={3}>
@@ -74,6 +98,7 @@ function App() {
   const [state, setState] = useState({}); //{ runState: "stopped" });
   const [start, setStart] = useState(false);
   const [val, setVal] = useState();
+  const [handleConnect, setHandleConnect] = useState(false);
 
   // When the component mounts, check that the browser supports Bluetooth
   useEffect(() => {
@@ -84,8 +109,8 @@ function App() {
 
   const plan = [
     { interval: 20, target: 100 },
-    { interval: 20, target: 150 },
     { interval: 20, target: 200 },
+    { interval: 20, target: 300 },
   ];
 
   useEffect(() => {
@@ -101,20 +126,23 @@ function App() {
     const prevData = state.data;
 
     prevData[diff] = { ...prevData[diff], ...val };
-    setState({ ...prevState, data: [...prevData] });
+    /* console.log("target", prevData[diff].target); */
+    const currentTarget = prevData[diff].target;
+    console.log("setting", currentTarget);
+    /* const x = requestControl(); */
+    /* console.log(x); */
+    /* await val.targetCb(); */
+    /* let power = 100; */
+
+    /* console.log(val.cb); */
+    /* await writeCharacteristic.writeValue(powerTarget({ power })); */
+    /* await val.targetCb(powerTarget({ power })); */
+    targetPowerLevel = currentTarget;
+    setState({
+      ...prevState,
+      data: [...prevData],
+    });
   }, [val]);
-
-  const handleCharacteristicValueChanged = (event) => {
-    let dataview = new DataView(event.target.value.buffer);
-
-    const speed = dataview.getUint16(2, true) / 100;
-    const cadence = dataview.getUint16(4, true) * 0.5;
-    const power = dataview.getInt16(6, true);
-
-    /* const inVal = dataview.getUint16(2, dataview, true); */
-    const dt = new Date();
-    setVal({ dt: dt, power, speed, cadence });
-  };
 
   useEffect(() => {
     if (!start) return;
@@ -144,40 +172,83 @@ function App() {
     setStart(false);
   }, [start]);
 
-  const connectToDeviceAndSubscribeToUpdates = async () => {
-    // Search for Bluetooth devices that advertise a battery service
-    const device = await navigator.bluetooth.requestDevice({
-      filters: [{ namePrefix: "KI" }],
-      optionalServices: ["00001826-0000-1000-8000-00805f9b34fb"],
-    });
+  useEffect(() => {
+    if (!handleConnect) return;
 
-    setIsConnected(true);
+    const connectToDeviceAndSubscribeToUpdates = async () => {
+      // Search for Bluetooth devices that advertise a battery service
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{ namePrefix: "KI" }],
+        optionalServices: ["00001826-0000-1000-8000-00805f9b34fb"],
+      });
 
-    device.addEventListener("gattserverdisconnected", () =>
-      setIsConnected(false)
-    );
+      setIsConnected(true);
 
-    const server = await device.gatt.connect();
+      device.addEventListener("gattserverdisconnected", () =>
+        setIsConnected(false)
+      );
 
-    // Get the battery service from the Bluetooth device
-    const service = await server.getPrimaryService(
-      "00001826-0000-1000-8000-00805f9b34fb"
-    );
+      const server = await device.gatt.connect();
 
-    // Get the battery level characteristic from the Bluetooth device
-    const characteristic = await service.getCharacteristic(
-      "00002ad2-0000-1000-8000-00805f9b34fb"
-    );
+      // Get the battery service from the Bluetooth device
+      const service = await server.getPrimaryService(
+        "00001826-0000-1000-8000-00805f9b34fb"
+      );
 
-    // Subscribe to battery level notifications
-    characteristic.startNotifications();
+      // Get the battery level characteristic from the Bluetooth device
+      const readCharacteristic = await service.getCharacteristic(
+        "00002ad2-0000-1000-8000-00805f9b34fb"
+      );
 
-    // When the battery level changes, call a function
-    characteristic.addEventListener(
-      "characteristicvaluechanged",
-      handleCharacteristicValueChanged
-    );
-  };
+      const writeCharacteristic = await service.getCharacteristic(
+        "00002ad9-0000-1000-8000-00805f9b34fb"
+      );
+
+      // Subscribe to battery level notifications
+      readCharacteristic.startNotifications();
+
+      const handleCharacteristicValueChanged = (event) => {
+        let dataview = new DataView(event.target.value.buffer);
+
+        const speed = dataview.getUint16(2, true) / 100;
+        const cadence = dataview.getUint16(4, true) * 0.5;
+        const power = dataview.getInt16(6, true);
+        console.log("to set", targetPowerLevel);
+
+        writeCharacteristic.writeValue(
+          powerTarget({ power: targetPowerLevel })
+        );
+        /* const inVal = dataview.getUint16(2, dataview, true); */
+        const dt = new Date();
+        setVal({
+          dt: dt,
+          power,
+          speed,
+          cadence,
+        });
+      };
+      // When the battery level changes, call a function
+      readCharacteristic.addEventListener(
+        "characteristicvaluechanged",
+        /* (event) => */
+        /* handleCharacteristicValueChanged({ */
+        /* event: event, */
+        /* cb: writeCharacteristic.writeValue, */
+        /* }) */
+        handleCharacteristicValueChanged
+      );
+
+      ////////////////////
+
+      await writeCharacteristic.writeValue(requestControl());
+
+      /* let power = 100; */
+      /* await writeCharacteristic.writeValue(powerTarget({ power })); */
+    };
+    connectToDeviceAndSubscribeToUpdates();
+
+    setHandleConnect(false);
+  }, [handleConnect, state]);
 
   return (
     <div>
@@ -189,7 +260,7 @@ function App() {
       {supportsBluetooth && !isConnected && (
         <div>
           <Button
-            onClick={connectToDeviceAndSubscribeToUpdates}
+            onClick={() => setHandleConnect(true)}
             variant="contained"
             startIcon={<BluetoothSearchingRoundedIcon />}
           >
